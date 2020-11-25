3af7ca1e4f222702f628045f370b35d0ea3bab25
Jack Dewhirst <jackdewhirst@gmail.com>
2020-11-25 12:34:15 +0000

    Added blog post views and urls

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from blog.models import Post, Comment
+from blog.forms import PostForm, CommentForm
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import (TemplateView, ListView,
+                            DetailView, CreateView,
+                            UpdateView, DeleteView)
 
 # Create your views here.
+
+class AboutView(TemplateView):
+    template_name = 'about.html' 
+    
+class PostListView(ListView):
+    model = Post
+    
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+        
+class PostDetailView(DetailView):
+    model = Post
+    
+class CreatePostView(LoginRequiredMixin, CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    model = Post
+    form_class = PostForm
+    
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    model = Post
+    form_class = PostForm
+    
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy('post_list')
+    
+class PostDraftView(LoginRequiredMixin, ListView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_list.html'
+    model = Post
+    
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True).orderby('created_date')
